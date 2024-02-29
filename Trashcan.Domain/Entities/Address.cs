764142aae10ec8f151219ad84f2c311286cf929f
Максим <ZavalishinMaxim@mail.ru>
2024-02-29 10:19:45 +0400

    Настройка сущностей "Address" и "Address_base"

--- Trashcan.Domain/Entities/Address.cs
+++ Trashcan.Domain/Entities/Address.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Trashcan.Domain.Entities.BaseEntity;
 
 namespace Trashcan.Domain.Entity
 {
-	internal class Address
+	/// <summary>
+	/// Адрес проишествия.
+	/// </summary>
+	public class Address : IBaseEntity<int>
 	{
+		/// <summary>
+		/// Уникальный идентификатор.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Координаты долготы на карте.
+		/// </summary>
+		public float Longitude { get; set; }
+
+		/// <summary>
+		/// Координаты широты на карте.
+		/// </summary>
+		public float Width { get; set; }
+
+		/// <summary>
+		/// Город, в котором произошло проишествие.
+		/// </summary>
+		public char City { get; set; }
+
+		/// <summary>
+		/// Название улицы, на которой произошло проишествие.
+		/// </summary>
+		public char Street { get; set; }
+
+		/// <summary>
+		/// Номер дома на улице, в котором произошло проишествие.
+		/// </summary>
+		public char House { get; set; }
 	}
 }
--- Trashcan.Domain/Entities/Address_base.cs
+++ Trashcan.Domain/Entities/Address_base.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Trashcan.Domain.Entities.BaseEntity;
 
 namespace Trashcan.Domain.Entity
 {
-	internal class Address_base
+	/// <summary>
+	/// База данных адресов, где будут заложены все адреса и определены координаты, где работает наш проект.
+	/// </summary>
+	public class Address_base : IBaseEntity<int>
 	{
+		/// <summary>
+		/// Уникальный идентификатор.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Координаты долготы на карте.
+		/// </summary>
+		public float Longitude { get; set; }
+
+		/// <summary>
+		/// Координаты широты на карте.
+		/// </summary>
+		public float Width { get; set; }
+
+		/// <summary>
+		/// Название города, в котором работает проект.
+		/// </summary>
+		public char City { get; set; }
+
+		/// <summary>
+		/// Название улицы, которая входит в зону работы проекта.
+		/// </summary>
+		public char Street { get; set; }
+
+		/// <summary>
+		/// Номер дома на улице, входящей в зону работы проекта.
+		/// </summary>
+		public char House { get; set; }
 	}
 }
