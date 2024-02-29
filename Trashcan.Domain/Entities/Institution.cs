1f48c76ca5a5e3a20388856ab4e9c9dfa25edbd6
Максим <ZavalishinMaxim@mail.ru>
2024-02-29 10:58:35 +0400

    Настройка сущности "Institution"

--- Trashcan.Domain/Entities/Institution.cs
+++ Trashcan.Domain/Entities/Institution.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Trashcan.Domain.Entities.BaseEntity;
 
 namespace Trashcan.Domain.Entity
 {
-	internal class Institution
+	/// <summary>
+	/// Инстанция, для которой может создаваться сообщение.
+	/// </summary>
+	public class Institution : IBaseEntity<int>
 	{
+		/// <summary>
+		/// Уникальный идентификатор.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Название инстанции, для которой создается сообщение.
+		/// </summary>
+		public char Name { get; set; }
+
+		/// <summary>
+		/// Город, в котором действует инстанция.
+		/// </summary>
+		public char City { get; set; }
+
+		/// <summary>
+		/// Район города, в котором действует инстанция.
+		/// </summary>
+		public char District { get; set; }
+
+		/// <summary>
+		/// Описание конкретной инстанции.
+		/// </summary>
+		public char Description { get; set; }
 	}
 }
