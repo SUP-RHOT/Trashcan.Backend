fd2b86b4a83fed15cc1c4a610bd2eed1c3ef74ed
Максим <ZavalishinMaxim@mail.ru>
2024-02-29 10:39:51 +0400

    Настройка сущности "Event", добовление коментариев для "Actor"

--- Trashcan.Domain/Entities/Actor.cs
+++ Trashcan.Domain/Entities/Actor.cs
@@ -3,34 +3,79 @@
 
 namespace Trashcan.Domain.Entity
 {
+	/// <summary>
+	/// Актёр, действующий субъект.
+	/// </summary>
 	public class Actor : IBaseEntity<int>
 	{
+		/// <summary>
+		/// Уникальный идентификатор.
+		/// </summary>
 		public int Id { get; set; }
 
+		/// <summary>
+		/// Фамилия субъекта.
+		/// </summary>
 		public char Lastname { get; set; }
 
+		/// <summary>
+		/// Имя субъекта.
+		/// </summary>
 		public char Firstname { get; set; }
 
+		/// <summary>
+		/// Отчество субъекта.
+		/// </summary>
 		public char Secondname { get; set; }
 
+		/// <summary>
+		/// Телефон субъекта.
+		/// </summary>
 		public char PhoneNumber { get; set; }
 
+		/// <summary>
+		/// E-mail субъекта.
+		/// </summary>
 		public char Email { get; set; }
 
+		/// <summary>
+		/// Город, в котором проживает субъект.
+		/// </summary>
 		public char City { get; set; }
 
+		/// <summary>
+		/// Улица, на которой проживает субъект.
+		/// </summary>
 		public char Street { get; set; }
 
+		/// <summary>
+		/// Дом, в котором проживает субъект.
+		/// </summary>
 		public char House { get; set; }
 
+		/// <summary>
+		/// Квартира, в которой проживает субъект.
+		/// </summary>
 		public char Apartament { get; set; }
 
+		/// <summary>
+		/// Логин субъекта.
+		/// </summary>
 		public char Login { get; set; }
 
+		/// <summary>
+		/// Пароль субъекта.
+		/// </summary>
 		public char Password { get; set; }
 
+		/// <summary>
+		/// Статус субъекта.
+		/// </summary>
 		public bool Status { get; set; }
 
+		/// <summary>
+		/// Согласие на получение рассылки новостей о проекте.
+		/// </summary>
 		public bool Mailing { get; set; }
 	}
 }
--- Trashcan.Domain/Entities/Event.cs
+++ Trashcan.Domain/Entities/Event.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Trashcan.Domain.Entities.BaseEntity;
 
 namespace Trashcan.Domain.Entity
 {
-	internal class Event
+	/// <summary>
+	/// Происшествие, которое будет описывать субъект.
+	/// </summary>
+	public class Event : IBaseEntity<int>
 	{
+		/// <summary>
+		/// Уникальный идентификатор.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Статус сообщения: черновик или готовое.
+		/// </summary>
+		public bool Status { get; set; }
+
+		/// <summary>
+		/// Тема сообщения.
+		/// </summary>
+		public char TypeMessage { get; set; }
+
+		/// <summary>
+		/// Текст субъекта, описывающий проблему.
+		/// </summary>
+		public char TextMessage { get; set; }
+
+		/// <summary>
+		/// Ссылка хранения фотографии событии.
+		/// </summary>
+		public string Photo { get; set; }
+
+		/// <summary>
+		/// Дата создания сообщения.
+		/// </summary>
+		public DateTime Date { get; set; }
+
+		/// <summary>
+		/// Результат решения проблемы.
+		/// </summary>
+		public bool Result { get; set; }
 	}
 }
