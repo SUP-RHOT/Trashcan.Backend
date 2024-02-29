6cde4e6cef93915e3a34d04ae9784f2ce61df50a
Максим <ZavalishinMaxim@mail.ru>
2024-02-29 10:44:42 +0400

    Настройка сущности "Rubric"

--- Trashcan.Domain/Entities/Rubric.cs
+++ Trashcan.Domain/Entities/Rubric.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Trashcan.Domain.Entities.BaseEntity;
 
 namespace Trashcan.Domain.Entity
 {
-	internal class Rubric
+	/// <summary>
+	/// Каталог или рубрикатор происшествий, инцидентов.
+	/// </summary>
+	public class Rubric : IBaseEntity<int>
 	{
+		/// <summary>
+		/// Уникальный идентификатор.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Название пункта каталога происшествий.
+		/// </summary>
+		public char Name { get; set; }
+
+		/// <summary>
+		/// Краткое описание пункта каталога происшествий.
+		/// </summary>
+		public char Description { get; set; }
+
+		/// <summary>
+		/// Ссылка хранения изображения темы.
+		/// </summary>
+		public char Image { get; set; }
 	}
 }
