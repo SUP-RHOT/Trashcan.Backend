688cfc4db9211f9d86b6d878892355ec84d22fad
Максим <ZavalishinMaxim@mail.ru>
2024-02-29 10:51:46 +0400

    Настройка сущности "Template"

--- Trashcan.Domain/Entities/Template.cs
+++ Trashcan.Domain/Entities/Template.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Trashcan.Domain.Entities.BaseEntity;
 
 namespace Trashcan.Domain.Entity
 {
-	internal class Template
+	/// <summary>
+	/// Шаблон сообщения, для обращения в соответствующую происшествию инстанцию.
+	/// </summary>
+	public class Template : IBaseEntity<int>
 	{
+		/// <summary>
+		/// Уникальный идентификатор.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Название шаблона сообщения, создаваемого для конкретной инстанции.
+		/// </summary>
+		public char Name { get; set; }
+
+		/// <summary>
+		/// Текст шаблона сообщения, создаваемого для конкретной инстанции.
+		/// </summary>
+		public char Text { get; set; }
 	}
 }
