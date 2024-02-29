d24c21ef981af40a9cf1c39d43a76210b0da7474
Максим <ZavalishinMaxim@mail.ru>
2024-02-29 10:03:02 +0400

    Настройка сущности "Actor"

--- Trashcan.Domain/Entities/Actor.cs
+++ Trashcan.Domain/Entities/Actor.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Trashcan.Domain.Entities.BaseEntity;
+
 
 namespace Trashcan.Domain.Entity
 {
-	internal class Actor
+	public class Actor : IBaseEntity<int>
 	{
+		public int Id { get; set; }
+
+		public char Lastname { get; set; }
+
+		public char Firstname { get; set; }
+
+		public char Secondname { get; set; }
+
+		public char PhoneNumber { get; set; }
+
+		public char Email { get; set; }
+
+		public char City { get; set; }
+
+		public char Street { get; set; }
+
+		public char House { get; set; }
+
+		public char Apartament { get; set; }
+
+		public char Login { get; set; }
+
+		public char Password { get; set; }
+
+		public bool Status { get; set; }
+
+		public bool Mailing { get; set; }
 	}
 }
